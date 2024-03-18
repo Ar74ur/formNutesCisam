9bce76c2ca916a3caa79de3a47ca6d13557da8b5
Azyhu <114199062+Ar74ur@users.noreply.github.com>
2024-03-18 20:56:10 -0300

    Update agendamentos.sql

--- nutes/agendamentos.sql
+++ nutes/agendamentos.sql
@@ -43,17 +43,6 @@ CREATE TABLE `agendamentos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `agendamentos`
---
-
-INSERT INTO `agendamentos` (`id`, `nome`, `tel`, `email`, `endereco`, `cep`, `cpf`, `data_nasc`, `nome_mae`, `cns`, `tipo`, `extra`) VALUES
-(7, 'maxiane', '87 88112255', 'maxiane@mail.com', 'rua 1, Recife - PE (Apt 05)', '12.345-678', '145.236-78', '25/09/20202', '', '', 'Climatério', 'Sintomas: \r\nAlterações nos órgãos sexuais, como coceira, secura da mucosa vaginal'),
-(11, 'maria', '88 85581225', 'maria@mail.com', 'rua 2, Recife - PE(bloco 2)', '25.565-555', '123.321-77', '11/09/2001', '', '', 'Climatério', 'Sintomas:\r\nDepressão ou irritabilidade'),
-(13, 'maria joaquina', '87 81323154', 'mj@email.com', 'tes, tes, Recife - PE', '50650458', '1255465415221', '11/09/2001', 'joana', '15wd14ew5', 'Prevenção da Gravidez', 'Inserção DIU Mirena (hormonal)'),
-(14, 'maria joaquina teste', '87 81323154', 'mj@email.com', 'tes, tes, Recife - PE', '50650458', '1255465415221', '11/09/2001', '', '', 'Prevenção da Gravidez', 'Inserção DIU Mirena (hormonal)</br>Inserção DIU Mirena (hormonal)'),
-(16, 'maria', '87 81224536', 'a@a.a', 'rua 1, teste, recife - pe(apt po)', '50564452', '125.252.698-88', '25/02/2000', 'dona zefa', '151fht150htg', 'Climatério', 'Selecione abaixo os sinais e sintomas que você está apresentando, pode clicar em mais de um:: Depressão ou irritabilidade');
-
---
 -- Índices para tabelas despejadas
 --
 
